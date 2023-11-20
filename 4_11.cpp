190a32987d36b98adfb0608aca3f47f237ca67f2
YDidkivskiy <y.didkivskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-20 14:08:31 +0200

    Завдання 4 № 11

--- 4_11.cpp
+++ 4_11.cpp
@@ -7,7 +7,9 @@ int main () {
     const double k = 7.2;
     double x = 5.0;
     double c = atan(x);
+
     double a = c + pow(k,2);
+
     double y = pow(sin(a),3) + pow(cos(2),2);
 
     std::cout << "c = " << c << "\na = " << a << "\ny = " << y;
